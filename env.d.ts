b53a8295b612b6ee5c6e3c9f190bb1548bab8295
“Nidhi <nidhib@zignuts.com>
2023-12-12 12:11:43 +0530

    stater-kit-vue3

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="vite/client" />
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    action?: string
+    subject?: string
+    layoutWrapperClasses?: string
+    navActiveLink?: RouteLocationRaw
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+/* eslint-disable import/order */
+import '@/@iconify/icons-bundle'
+import App from '@/App.vue'
+import layoutsPlugin from '@/plugins/layouts'
+import vuetify from '@/plugins/vuetify'
+import { loadFonts } from '@/plugins/webfontloader'
+import router from '@/router'
+import '@core/scss/template/index.scss'
+import '@styles/styles.scss'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
-import App from './App.vue'
-import router from './router'
+loadFonts()
 
+// Create vue app
 const app = createApp(App)
 
+// Use plugins
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
+app.use(layoutsPlugin)
 
+// Mount vue app
 app.mount('#app')
